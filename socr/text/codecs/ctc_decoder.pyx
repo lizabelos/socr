d78d9468061129922d5a592bb5687918e90efc5e
Belos Thomas <belos.thomas@outlook.com>
2018-08-31 15:37:47 +0200

    Travix fix

--- socr/text/codecs/ctc_decoder.pyx
+++ socr/text/codecs/ctc_decoder.pyx
@@ -42,43 +42,43 @@ cdef class CTCDecoder:
         return text
     
     
-    cpdef beam_bi_decode(self, float[:,:,:] sequence):
-        cdef int num_ngram = 64
-
-        if sequence.shape[2] != self.label_len:
-            print_error(str(sequence.shape) + "!=" + str(self.label_len))
-            assert False
-
-        cdef int width = sequence.shape[1]
-        cdef int batch_size = sequence.shape[0]
-
-        cdef str text = ""
-        cdef int last_label = -1
-
-        cdef int time
-        cdef int max_label
-        cdef int i
-
-        cdef list last_beams = [(1.0,[])]
-        cdef list current_beams
-
-        for time in range(0, width):
-
-            current_beams = []
-
-            for beam in last_beams:
-                for i in range(0, self.label_len):
-                    # TODO : blank and repetition !
-                    # TODO : take care about the restriction
-                    current_beams.append((beam[0] * sequence[0][time][i],beam[1] + [self.inv_labels[i]]))
-
-
-            last_beams = sorted(current_beams, reverse=True, key=lambda x: x[0])[:num_ngram]
-
-
-
-
-            # TODO : think to normalize
-
-
-        return last_beams[0][1]
+    # cpdef beam_bi_decode(self, float[:,:,:] sequence):
+    #     cdef int num_ngram = 64
+    #
+    #     if sequence.shape[2] != self.label_len:
+    #         print_error(str(sequence.shape) + "!=" + str(self.label_len))
+    #         assert False
+    #
+    #     cdef int width = sequence.shape[1]
+    #     cdef int batch_size = sequence.shape[0]
+    #
+    #     cdef str text = ""
+    #     cdef int last_label = -1
+    #
+    #     cdef int time
+    #     cdef int max_label
+    #     cdef int i
+    #
+    #     cdef list last_beams = [(1.0,[])]
+    #     cdef list current_beams
+    #
+    #     for time in range(0, width):
+    #
+    #         current_beams = []
+    #
+    #         for beam in last_beams:
+    #             for i in range(0, self.label_len):
+    #                 # TODO : blank and repetition !
+    #                 # TODO : take care about the restriction
+    #                 current_beams.append((beam[0] * sequence[0][time][i],beam[1] + [self.inv_labels[i]]))
+    #
+    #
+    #         last_beams = sorted(current_beams, reverse=True, key=lambda x: x[0])[:num_ngram]
+    #
+    #
+    #
+    #
+    #         # TODO : think to normalize
+    #
+    #
+    #     return last_beams[0][1]
